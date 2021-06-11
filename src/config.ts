5597940e3e05fa52715215450cae53f7ea37895a
Jakub Jastrzębski <jakub.jastrzebski@pm.me>
2021-06-11 09:41:27 +0200

    fix: don't log errors when using default config (#22)

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,9 @@ export async function readUserConfig(configPath: string): Promise<Config> {
   try {
     return require(configPath);
   } catch (err) {
-    console.error(err);
+    if (err.code !== 'MODULE_NOT_FOUND') {
+      console.error(err);
+    }
     console.log('Using default config');
   }
   return {};
