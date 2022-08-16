206652ff624cb2bfce66beaf40439078dd7ff1f7
Atul R <atulanand94@gmail.com>
2022-08-17 00:03:23 +0200

    Adds file url support for windows (#50)
    * Adds file url support for windows
    
    * 2.0.2

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-import fs from "fs";
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
 import type { BuildOptions as EsBuildOptions } from "esbuild";
 
 export type Config = Partial<{
@@ -11,7 +13,10 @@ export type Config = Partial<{
 export async function readUserConfig(configPath: string): Promise<Config> {
   if (fs.existsSync(configPath)) {
     try {
-      const { default: config } = await import(configPath);
+      const configPathUrl = path.isAbsolute(configPath)
+        ? url.pathToFileURL(configPath).toString()
+        : configPath;
+      const { default: config } = await import(configPathUrl);
       if (config.outDir) {
         console.warn(
           "Your etsc config file is using the old v1.0 format. Please update to the new v2.0 format. Check https://github.com/a7ul/esbuild-node-tsc for details."
